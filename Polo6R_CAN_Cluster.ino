2e79df0e063e09b2555dd53f945a28d22acf8aca
Doctor Ned <1933864+Doctor-Ned@users.noreply.github.com>
2022-01-01 21:28:56 +0100

    Massive QoL improvements

--- Polo6R_CAN_Cluster.ino
+++ Polo6R_CAN_Cluster.ino
@@ -4,28 +4,19 @@
 #define SPI_CS_PIN 10
 #define CAN_ID_MODE MCP_ANY
 #define CAN_SPEED CAN_500KBPS
-#define CAN_CLOCK MCP_16MHZ
-
-#define lo8(x) ((int)(x)&0xff)
-#define hi8(x) ((int)(x) >> 8)
+#define CAN_CLOCK MCP_8MHZ
+#define CAN_REFRESH_RATE_US 50000UL
 
 #define HANDBRAKE_CONTROL_PIN 9
 #define FOG_LIGHT_INDICATOR_PIN 8
 #define HIGH_BEAM_INDICATOR_PIN 7
 
-#define DEFINE_SETTER(setter_name, field_type, field_name, send_method_name) \
-    void setter_name(field_type field_name)                                  \
-    {                                                                        \
-        dashboard.field_name = field_name;                                   \
-        /* send_method_name(); */                                            \
-    }
-
-#define DEFINE_BSETTER(setter_name, field_name, send_method_name) \
-    DEFINE_SETTER(setter_name, bool, field_name, send_method_name)
+#define MAX_RPM 6000U
+#define MAX_KMH 240U
 
 struct DashboardSettings
 {
-    unsigned short speed = 0;         // speed, enough precision for about 491 [km/h]
+    unsigned short speed = 0;         // speed [km/h]
     unsigned short rpm = 0;           // revs [rpm]
     bool backlight = true;            // dashboard backlight (on/off)
     byte turning_lights = 0;          // turning lights (0: off, 1: left, 2: right, 3: both)
@@ -41,45 +32,15 @@ struct DashboardSettings
     bool key_battery_warning = false; // display 'key battery low' message on dashboard display. probably works only at start (on/off)
     bool fog_light = false;           // fog light lamp (on/off)
     bool high_beam = false;           // high beam lamp (on/off)
-    bool seat_belt_warning = false;   // seat belt warning lamp (on/off) PLEASE DISABLE IT, IT BEEPS SO LOUDLY ;_;
+    bool seat_belt_warning = false;   // seat belt warning lamp (on/off)
     bool preheating = false;          // diesel preheating lamp (on/off)
-    bool high_water_temp = false;     // high water temperature lamp (on/off)
+    bool high_water_temp = false;     // high water temperature lamp (on/off) AVOID IT PLEASE, BEEPS AWFULLY LOUD
     bool dpf_warning = false;         // DPF warning lamp (on/off)
 };
 
 MCP_CAN can(SPI_CS_PIN);
-
 DashboardSettings dashboard;
-
-const unsigned long CAN_REFRESH_RATE = 20UL;
-const unsigned long CAN_LIGHT_ENGINE_REFRESH_RATE = 200UL;
-unsigned long last_dashboard_millis = 0UL;
-unsigned long last_can_millis = 0UL;
-unsigned long last_can_light_engine_millis = 0UL;
-bool is_set = false;
-
-void setup()
-{
-    pinMode(HANDBRAKE_CONTROL_PIN, OUTPUT);
-    pinMode(FOG_LIGHT_INDICATOR_PIN, OUTPUT);
-    pinMode(HIGH_BEAM_INDICATOR_PIN, OUTPUT);
-    digitalWrite(HANDBRAKE_CONTROL_PIN, !dashboard.handbrake ? HIGH : LOW);
-    digitalWrite(FOG_LIGHT_INDICATOR_PIN, dashboard.fog_light ? HIGH : LOW);
-    digitalWrite(HIGH_BEAM_INDICATOR_PIN, dashboard.high_beam ? HIGH : LOW);
-
-    Serial.begin(115200);
-    byte can_result = can.begin(CAN_ID_MODE, CAN_SPEED, CAN_CLOCK);
-    while (can_result != CAN_OK)
-    {
-        Serial.println("CAN initialization failed! Retrying...");
-        delay(250);
-        can_result = can.begin(CAN_ID_MODE, CAN_SPEED, CAN_CLOCK);
-    }
-    Serial.println("CAN initialized!");
-    can.setMode(MCP_NORMAL);
-
-    last_dashboard_millis = last_can_millis = last_can_light_engine_millis = millis();
-}
+unsigned long last_can_us = 0UL;
 
 void canSend(short address, byte a, byte b, byte c, byte d, byte e, byte f, byte g, byte h)
 {
@@ -97,12 +58,15 @@ void sendEngineOnAndESP()
     canSend(0xDA0, 0x01, 0x80, 0, 0, 0, 0, 0, 0);
 }
 
-void sendSpeedAndDriveMode()
+void sendSpeedRPMDriveMode()
 {
-    int speed = static_cast<unsigned short>(static_cast<float>(dashboard.speed) / 0.0075f);
+    int speed = static_cast<unsigned short>(static_cast<float>(dashboard.speed) / 0.007f);
     byte speedLow = speed & 0xFF, speedHigh = (speed >> 8) & 0xFF;
     // motorspeed
-    canSend(0x320, 0, speedLow * 100, speedHigh * 100, 0, 0, 0, 0, 0);
+    canSend(0x320, 0, speedLow, speedHigh, 0, 0, 0, 0, 0);
+    // rpm
+    unsigned short rpm = dashboard.rpm * 4;
+    canSend(0x280, 0x49, 0x0E, rpm & 0xFF, (rpm >> 8) & 0xFF, 0, 0, 0, 0);
     byte drive_mode = 0;
     if (dashboard.abs)
     {
@@ -122,14 +86,9 @@ void sendSpeedAndDriveMode()
     }
     // actual speed and drivemode, could contain mileage counter as byte 5 and 6 (0-indexed)
     canSend(0x5A0, 0xFF, speedLow, speedHigh, drive_mode, 0, 0, 0, 0xAD);
+    digitalWrite(HANDBRAKE_CONTROL_PIN, dashboard.handbrake ? LOW : HIGH);
     // for ABS
     canSend(0x1A0, 0x18, speedLow, speedHigh, 0, 0xFE, 0xFE, 0, 0xFF);
-}
-
-void sendRPM()
-{
-    unsigned short rpm = dashboard.rpm * 4;
-    canSend(0x280, 0x49, 0x0E, rpm & 0xFF, (rpm >> 8) & 0xFF, 0, 0, 0, 0);
 }
 
 void sendSeatBeltWarning()
@@ -207,130 +166,120 @@ void sendLights()
         b7 |= 0x40;
     }
     canSend(0x470, b0, b1, b2, 0, b4, b5, 0, b7);
-}
-
-DEFINE_SETTER(setSpeed, unsigned short, speed, sendSpeedAndDriveMode)
-DEFINE_BSETTER(setABS, abs, sendSpeedAndDriveMode)
-DEFINE_BSETTER(setOffroad, offroad, sendSpeedAndDriveMode)
-
-void setHandbrake(bool handbrake)
-{
-    dashboard.handbrake = handbrake;
-    digitalWrite(HANDBRAKE_CONTROL_PIN, !handbrake ? HIGH : LOW);
-    // sendSpeedAndDriveMode();
-}
-
-DEFINE_BSETTER(setLowTirePressure, low_tire_pressure, sendSpeedAndDriveMode)
-DEFINE_SETTER(setRPM, unsigned short, rpm, sendRPM)
-DEFINE_BSETTER(setSeatBeltWarning, seat_belt_warning, sendSeatBeltWarning)
-DEFINE_BSETTER(setPreheating, preheating, sendEngineControl)
-DEFINE_BSETTER(setHighWaterTemperature, high_water_temp, sendEngineControl)
-DEFINE_BSETTER(setDPFWarning, dpf_warning, sendEngineControl)
-DEFINE_BSETTER(setBatteryWarning, battery_warning, sendLights)
-DEFINE_BSETTER(setDoorOpenWarning, door_open_warning, sendLights)
-DEFINE_BSETTER(setTrunkOpenWarning, trunk_open_warning, sendLights)
-DEFINE_BSETTER(setBacklight, backlight, sendLights)
-DEFINE_BSETTER(setClutchControl, clutch_control, sendLights)
-DEFINE_BSETTER(setCheckLamp, check_lamp, sendLights)
-DEFINE_BSETTER(setKeyBatteryWarning, key_battery_warning, sendLights)
-
-void setFogLight(bool fog_light)
-{
-    dashboard.fog_light = fog_light;
-    digitalWrite(FOG_LIGHT_INDICATOR_PIN, fog_light ? HIGH : LOW);
-    // sendLights();
-}
-
-void setHighBeam(bool high_beam)
-{
-    dashboard.high_beam = high_beam;
-    digitalWrite(HIGH_BEAM_INDICATOR_PIN, high_beam ? HIGH : LOW);
-    // sendLights();
+    digitalWrite(FOG_LIGHT_INDICATOR_PIN, dashboard.fog_light ? HIGH : LOW);
+    digitalWrite(HIGH_BEAM_INDICATOR_PIN, dashboard.high_beam ? HIGH : LOW);
 }
 
 void updateDashboard()
 {
-    unsigned long ms = millis();
-    unsigned long diff = ms - last_can_millis;
-    if (diff >= CAN_REFRESH_RATE)
+    unsigned long us = micros();
+    unsigned long diff = us - last_can_us;
+    if (diff >= CAN_REFRESH_RATE_US)
     {
-        last_can_millis += diff / CAN_REFRESH_RATE * CAN_REFRESH_RATE;
+        last_can_us += diff / CAN_REFRESH_RATE_US * CAN_REFRESH_RATE_US;
         sendImmobilizer();
         sendEngineOnAndESP();
-        diff = ms - last_can_light_engine_millis;
-        if (diff >= CAN_LIGHT_ENGINE_REFRESH_RATE)
-        {
-            last_can_light_engine_millis += diff / CAN_LIGHT_ENGINE_REFRESH_RATE * CAN_LIGHT_ENGINE_REFRESH_RATE;
-            sendLights();
-            sendEngineControl();
-        }
-        sendSpeedAndDriveMode();
-        sendRPM();
+        sendLights();
+        sendEngineControl();
+        sendSpeedRPMDriveMode();
         sendSeatBeltWarning();
     }
 }
 
-void unsetAll()
+void setup()
 {
-    dashboard.speed = 0;
-    dashboard.rpm = 0;
-    dashboard.backlight = false;
-    dashboard.turning_lights = 0;
-    dashboard.abs = false;
-    dashboard.offroad = false;
-    setHandbrake(false);
-    dashboard.low_tire_pressure = false;
-    dashboard.door_open_warning = false;
-    dashboard.clutch_control = false;
-    dashboard.check_lamp = false;
-    dashboard.trunk_open_warning = false;
-    dashboard.battery_warning = false;
-    dashboard.key_battery_warning = false;
-    setFogLight(false);
-    setHighBeam(false);
-    dashboard.seat_belt_warning = false;
-    dashboard.preheating = false;
-    dashboard.high_water_temp = false;
-    dashboard.dpf_warning = false;
-}
+    pinMode(HANDBRAKE_CONTROL_PIN, OUTPUT);
+    pinMode(FOG_LIGHT_INDICATOR_PIN, OUTPUT);
+    pinMode(HIGH_BEAM_INDICATOR_PIN, OUTPUT);
 
-void setAll()
-{
-    dashboard.speed = 100;
-    dashboard.rpm = 3000;
-    dashboard.backlight = true;
-    dashboard.turning_lights = 3;
-    dashboard.abs = true;
-    dashboard.offroad = true;
-    setHandbrake(true);
-    dashboard.low_tire_pressure = true;
-    dashboard.door_open_warning = true;
-    dashboard.clutch_control = true;
-    dashboard.check_lamp = true;
-    dashboard.trunk_open_warning = true;
-    dashboard.battery_warning = true;
-    dashboard.key_battery_warning = true;
-    setFogLight(true);
-    setHighBeam(true);
-    dashboard.seat_belt_warning = true;
-    dashboard.preheating = true;
-    dashboard.high_water_temp = true;
-    dashboard.dpf_warning = true;
+    Serial.begin(115200);
+    byte can_result = can.begin(CAN_ID_MODE, CAN_SPEED, CAN_CLOCK);
+    while (can_result != CAN_OK)
+    {
+        Serial.println("CAN initialization failed! Retrying...");
+        delay(250);
+        can_result = can.begin(CAN_ID_MODE, CAN_SPEED, CAN_CLOCK);
+    }
+    Serial.println("CAN initialized!");
+    can.setMode(MCP_NORMAL);
 }
 
 void loop()
 {
     updateDashboard();
-    if (Serial.available() > 0)
+    if (Serial.available())
     {
-        int i = Serial.parseInt();
-        if (i)
+        char c = Serial.read();
+        switch (c)
         {
-            setAll();
-        }
-        else
-        {
-            unsetAll();
+        case 'a':
+            dashboard.backlight = !dashboard.backlight;
+            break;
+        case 'b':
+            dashboard.abs = !dashboard.abs;
+            break;
+        case 'c':
+            dashboard.offroad = !dashboard.offroad;
+            break;
+        case 'd':
+            dashboard.handbrake = !dashboard.handbrake;
+            break;
+        case 'e':
+            dashboard.low_tire_pressure = !dashboard.low_tire_pressure;
+            break;
+        case 'f':
+            dashboard.door_open_warning = !dashboard.door_open_warning;
+            break;
+        case 'g':
+            dashboard.clutch_control = !dashboard.clutch_control; // "EMBRAY"
+            break;
+        case 'h':
+            dashboard.check_lamp = !dashboard.check_lamp;
+            break;
+        case 'i':
+            dashboard.trunk_open_warning = !dashboard.trunk_open_warning;
+            break;
+        case 'j':
+            dashboard.battery_warning = !dashboard.battery_warning;
+            break;
+        case 'k':
+            dashboard.key_battery_warning = !dashboard.key_battery_warning; // "PILECLE"
+            break;
+        case 'l':
+            dashboard.fog_light = !dashboard.fog_light;
+            break;
+        case 'm':
+            dashboard.high_beam = !dashboard.high_beam;
+            break;
+        case 'n':
+            dashboard.seat_belt_warning = !dashboard.seat_belt_warning;
+            break;
+        case 'o':
+            dashboard.preheating = !dashboard.preheating;
+            break;
+        case 'p':
+            dashboard.high_water_temp = !dashboard.high_water_temp;
+            break;
+        case 'q':
+            dashboard.dpf_warning = !dashboard.dpf_warning;
+            break;
+        case 'r':
+            dashboard.turning_lights = dashboard.turning_lights == 3 ? 0 : (dashboard.turning_lights + 1);
+            break;
+        case 's':
+            dashboard.speed += 50;
+            if (dashboard.speed > MAX_KMH)
+            {
+                dashboard.speed = 0;
+            }
+            break;
+        case 't':
+            dashboard.rpm += 500;
+            if (dashboard.rpm > MAX_RPM)
+            {
+                dashboard.rpm = 0;
+            }
+            break;
         }
     }
 }
